9ed6db7259b9133ab2c871cdbdb74f6d16063e87
belal ahmad <93271099+Salmannm@users.noreply.github.com>
2021-12-21 16:49:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ var darkmode = document.querySelector('#flexSwitchCheckDefault')
 var dark = document.querySelector('.dark')
 var black = document.querySelector('.black')
 var butto = document.querySelector('#btn4')
-// var clear = document.querySelector('.clear')
-// var text1 = document.querySelector('#text').value;
-
-
 
 function change() {
     let text = document.querySelector('#text').value.toUpperCase();
@@ -42,9 +38,4 @@ darkmode.addEventListener('click', () => {
 butto.addEventListener('click', () => {
     document.querySelector('#text').value = ""
 })
-// if (text1==""){
-//     clear.textContent=""
-// }
-// else{
-//     clear.textContent = text1
-// }
+
